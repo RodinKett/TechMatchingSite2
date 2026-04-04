30de9e490c4bfd282a604599626aab93e08dbc63
Emilie-04 <emilieelisabethe@gmail.com>
2026-04-04 04:10:11 +0200

    filter animatie

--- static/js/filter.js
+++ static/js/filter.js
@@ -1,13 +1,16 @@
 
-//   filter open en dicht kunnen doen 
-  const buttons = document.querySelectorAll(".filter-opties .button");
+// filter open en dicht + pijltje draaien
+const buttons = document.querySelectorAll(".filter-opties .button");
 
-  buttons.forEach(button => {
-    button.addEventListener("click", () => {
-      const parent = button.parentElement;
-      parent.classList.toggle("open");
-    });
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    const parent = button.parentElement;
+    parent.classList.toggle("open");
+
+    const pijltje = button.querySelector(".pijltje");
+    pijltje.classList.toggle("gedraaid");
   });
+});
 
 //   telling hoeveel filters er aan staan 
 const inputs = document.querySelectorAll(".filter-opties input, .filter-opties select");
