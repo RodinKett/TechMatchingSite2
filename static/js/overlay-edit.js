6d77dde18e3b4efb7a6ae746a1b71bf44387f5b7
Emilie-04 <emilieelisabethe@gmail.com>
2026-04-06 16:28:46 +0200

    .

--- static/js/overlay-edit.js
+++ static/js/overlay-edit.js
@@ -1,3 +1,10 @@
-document.getElementById("edit-profiel").addEventListener("click", () => {
-  document.getElementById("profiel-overlay").classList.toggle("verborgen");
+const overlay = document.getElementById("profiel-overlay");
+const openBtn = document.getElementById("button-edit-profiel");
+
+openBtn.addEventListener("click", () => {
+  overlay.classList.toggle("zichtbaar");
+});
+
+openBtn.addEventListener("click", () => {
+  overlay.classList.toggle("zichtbaar");
 });
